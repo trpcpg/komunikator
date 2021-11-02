a9c4054b92842ed055411530b18e731efff0730b
trpcpg <tomasz.bramowski@gmial.com>
2021-11-02 09:31:17 +0100

    project initialization

---
+++ src/main/java/net/trpcp/komunikator/KomunikatorApplication.java
@@ -0,0 +1,13 @@
+package net.trpcp.komunikator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomunikatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomunikatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/trpcp/komunikator/KomunikatorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.trpcp.komunikator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomunikatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
